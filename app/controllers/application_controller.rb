ee7a7434effb59b846001a37798bce4f324b1f88
Kittaka <46982975+majsxpwa@users.noreply.github.com>
2025-04-25 06:43:21 +0000

    edit controller and router

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
-
+  
   def hello
+    render html: "hello, world!"
+  end
+
+  def hola
     render html: "hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
   end
 end
